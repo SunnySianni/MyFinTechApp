2225d8b6a8d2ed37c0cd07c3cffdfef9369e34bb
SunnySianni <sstri0041@launchpadphilly.org>
2025-01-08 12:08:59 -0500

    final version fr

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ import http from 'http';
 
 // Setup Server and Application config 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 const server = http.createServer(app);
 
 // Define __dirname for ES modules
